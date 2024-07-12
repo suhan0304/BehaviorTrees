2c6dc27d8cc6f5c5495e98e1d856c4aa22de8941
ZerrY <suhan0304@gmail.com>
2024-07-12 16:32:38 +0900

    Update Timer.cs

--- BehaviorTrees/Assets/WUG/Scripts/Behaviors/Decorator/Timer.cs
+++ BehaviorTrees/Assets/WUG/Scripts/Behaviors/Decorator/Timer.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 using WUG.BehaviorTreeVisualizer;
 
 public class Timer : Decorator {
@@ -12,11 +13,32 @@ public class Timer : Decorator {
 
     protected override void OnReset() { }
     protected override NodeStatus OnRun() {
-        //Confirm that a valid child node was passed in the constructor
+        // Confirm that a valid child node was passed in the constructor
         if (ChildNodes.Count == 0 || ChildNodes[0] == null) {
             return NodeStatus.Failure;
         }
 
+        // Run the child node and calculate the elapsed
+        NodeStatus originalStatus = (ChildNodes[0] as Node).Run();
+
+        // If this is the first eval, then the start time needs to be set up
+        if (EvalutionCount == 0) {
+            StatusReason = $"Starting timer for {m_TimeToWait}. Child Node status is : {originalStatus}";
+            m_StartTime = m_UseFixedTime ? Time.fixedTime : Time.time;
+        }
+
+        //Calculate how much time has passed
+        float elapsedTime = Time.fixedTime - m_StartTime;
+
+        // If more time has passed than we wanted, it's time to stop
+        if (elapsedTime > m_TimeToWait) {
+            StatusReason = $"Time complete - Child node status is : {originalStatus}";
+            return NodeStatus.Success;
+        }
+
+        // Otherwise, keep running
+        StatusReason = $"Time is {elapsedTime} out of {m_TimeToWait}. Child node status is : {originalStatus}";
+        return NodeStatus.Running;
 
     }
 }
