2d6885bc9d30bb0a62c184261b90e94cc20a5c02
ZerrY <suhan0304@gmail.com>
2024-07-12 15:56:43 +0900

    Update

---
+++ BehaviorTrees/Assets/WUG/Scripts/Behaviors/Composite/Sequence.cs
@@ -0,0 +1,31 @@
+using WUG.BehaviorTreeVisualizer;
+
+public class Sequence : Composite
+{
+    public Sequence(string displayName, params Node[] childNodes) : base(displayName, childNodes) { }
+
+    protected override NodeStatus OnRun()
+    {
+        // Check the status of the last child
+        NodeStatus childNodeStatus = (ChildNodes[CurrentChildIndex] as Node).Run();
+
+        // Evaluate the current child node. If it's failed - sequence should fail.
+        switch (childNodeStatus) {
+            // Child failed - return failure
+            case NodeStatus.Failure :
+                return childNodeStatus;
+            // It Suceeded - move to the next child
+            case NodeStatus.Success :
+                CurrentChildIndex++;
+                break;
+        }
+
+        // All Children have run successfully - return success
+        if (CurrentChildIndex >= ChildNodes.Count) {
+            return NodeStatus.Success;
+        }
+
+        // The child was a success but we still have more to do - so call this method again
+        return childNodeStatus == NodeStatus.Success ? OnRun() : NodeStatus.Running;
+    }
+}
