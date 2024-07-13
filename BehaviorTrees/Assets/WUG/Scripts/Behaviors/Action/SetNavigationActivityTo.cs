d50830809dac842afcbb358e4ad66bda31b22067
ZerrY <suhan0304@gmail.com>
2024-07-13 15:12:24 +0900

    Update

--- BehaviorTrees/Assets/WUG/Scripts/Behaviors/Action/SetNavigationActivityTo.cs
+++ BehaviorTrees/Assets/WUG/Scripts/Behaviors/Action/SetNavigationActivityTo.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using WUG.BehaviorTreeDemo;
+using WUG.BehaviorTreeVisualizer;
 
-public class SetNavigationActivityTo : MonoBehaviour
+public class SetNavigationActivityTo : Node 
 {
-    // Start is called before the first frame update
-    void Start()
+    private NavigationActivity m_NewActicity;
+
+    public SetNavigationActivityTo (NavigationActivity newActicity)
     {
-        
+        m_NewActicity = newActicity;
+        Name = $"Set NavigationActivity to {m_NewActicity}";
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void OnReset() { }
+
+    protected override NodeStatus OnRun() 
     {
-        
+        if (GameManager.Instance == null || GameManager.Instance.NPC == null) {
+            StatusReason = "GameManager and/or NPC is null";
+            return NodeStatus.Failure;
+        }
+
+        GameManager.Instance.NPC.MyActivity = m_NewActicity;
+
+        return NodeStatus.Success;
     }
 }
