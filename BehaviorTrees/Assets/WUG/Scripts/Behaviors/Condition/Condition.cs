df5b82347804da93d46beedd2d3d50e390663078
ZerrY <suhan0304@gmail.com>
2024-07-13 14:49:31 +0900

    Update Condition.cs

--- BehaviorTrees/Assets/WUG/Scripts/Behaviors/Condition/Condition.cs
+++ BehaviorTrees/Assets/WUG/Scripts/Behaviors/Condition/Condition.cs
@@ -1,18 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Condition : MonoBehaviour
+public abstract class Condition : Node 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public Condition(string name) {
+        Name = name;
     }
 }
